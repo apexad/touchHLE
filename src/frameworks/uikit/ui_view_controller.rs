apexad/touchHLE

commit 124665d7eda989d8a12a1219fb289cc99ea8afd1

Set the UIViewController's view viewController property to nil before releasing the view when deallocating
Fixes regression in Resident Evil 4

Change-Id: Ifef849964ec05b6ad63eb286be770e7c88b4de0a

src/frameworks/uikit/ui_view_controller.rs
@@ -76,6 +76,7 @@ pub const CLASSES: ClassExports = objc_classes! {
 - (())dealloc {
     let &UIViewControllerHostObject { view, nib_name, bundle } = env.objc.borrow(this);
 
+    set_view_controller(env, view, nil);
     release(env, view);
     release(env, nib_name);
     release(env, bundle);

commit ad5ce73bbfc8b3a68f889c56aea34f9a7cacf605

Skip setting view controller for nil view in dealloc of UIViewController
Fixes regressions in Asphalt 6 and Spore introduced with https://github.com/touchHLE/touchHLE/commit/124665d7eda989d8a12a1219fb289cc99ea8afd1

Change-Id: Ibad12ff9c7ab29ce2919491f1dc67b389532b47b

src/frameworks/uikit/ui_view_controller.rs
@@ -76,7 +76,9 @@ pub const CLASSES: ClassExports = objc_classes! {
 - (())dealloc {
     let &UIViewControllerHostObject { view, nib_name, bundle } = env.objc.borrow(this);
 
-    set_view_controller(env, view, nil);
+    if view != nil {
+        set_view_controller(env, view, nil);
+    }
     release(env, view);
     release(env, nib_name);
     release(env, bundle);
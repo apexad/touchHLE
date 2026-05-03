ad5ce73bbfc8b3a68f889c56aea34f9a7cacf605
ciciplusplus <ciciplusplus@gmail.com>
2026-05-03 14:42:30 +0200

    Skip setting view controller for nil view in dealloc of UIViewController
    Fixes regressions in Asphalt 6 and Spore introduced with https://github.com/touchHLE/touchHLE/commit/124665d7eda989d8a12a1219fb289cc99ea8afd1
    
    Change-Id: Ibad12ff9c7ab29ce2919491f1dc67b389532b47b

--- src/frameworks/uikit/ui_view_controller.rs
+++ src/frameworks/uikit/ui_view_controller.rs
@@ -76,7 +76,9 @@ pub const CLASSES: ClassExports = objc_classes! {
 - (())dealloc {
     let &UIViewControllerHostObject { view, nib_name, bundle } = env.objc.borrow(this);
 
-    set_view_controller(env, view, nil);
+    if view != nil {
+        set_view_controller(env, view, nil);
+    }
     release(env, view);
     release(env, nib_name);
     release(env, bundle);
